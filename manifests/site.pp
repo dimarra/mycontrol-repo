fd979639067ac8ef92f985eca0381f2da2b70ea1
Raul Dimar <rdimar@shelde.com>
2016-11-11 15:36:20 +1100

    indicate source for the chocolatey package provider

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,7 +47,10 @@ node default {
     }
     'windows': {
       include chocolatey
-      Package { provider => chocolatey, }
+      Package { 
+        provider => chocolatey,
+        source => 'http://ip-172-31-25-24.ap-southeast-2.compute.internal/chocolatey',
+      }
     }
     default: {
       # do windows special here
